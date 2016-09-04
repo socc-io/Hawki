8a4dbfbc06dd457e4cbe589dbe8013a9c1d51085
becxer <becxer@beaver.com>
2016-09-04 22:09:45 +0900

    Update pipeline.py

--- Predictor/pipeline.py
+++ Predictor/pipeline.py
@@ -1,70 +1,73 @@
 
-# Preprocessor
-class APVOCA:
-    def __init__():
-        #Once Load data (only in start server)
-        pass
-
-    def resume():
-        #Always load data
-        pass
-    
-    def convert(data):
-        #Return custom data or vector
-        return data
-
 # Predictor
-class SIMPLE:
+class GNB:
 
-    def __init__():
+    def __init__(self):
+        import pickle
         #Once Load module (only in start server)
-        pass
+        with open('AlgoModel/model/gnb_x_0.pkl', 'rb') as f:
+            self.clf_x = pickle.load(f)
+        with open('AlgoModel/model/gnb_y_0.pkl', 'rb') as f:
+            self.clf_y = pickle.load(f)
 
-    def resume():
+    def resume(self):
         #Always load data (load each process)
         pass
 
-    def convert(vector):
+    def convert(self, vector):
         #Return ( x,y,z,confidence )
-        return [10,10,3,90]
+        preds_x = self.clf_x.predict(vector)
+        preds_y = self.clf_y.predict(vector)
+        print preds_x
+        print preds_y
+        return preds_x[0], preds_y[0]
 
-    
 PPlist = {
-    'APTOP25' : APVOCA(25),
-    'APTOP15' : APVOCA(15),
-    'SVM' : SVM(),
-    'BAYES' : BAYES()
+    'GNB' : GNB()
 }
 
 ##### BELOW IS PIPLINE #####
 
 class Pipeline:
-    
-    def __init__():
+
+    def __init__(self):
         self.pipes = {}
 
-    def load_pipes():
+    def load_pipes(self):
         # Export this to Config file
-        self.pipes['FIRST'] = ['APTOP25', 'SVM']
-        self.pipes['SECOND'] = ['APTOP15', 'BAYES']
-        self.pipes['THIRD'] = ['APTOP25', 'BAYES']
-        
-    def process(input_data):
+        self.pipes['GNB'] = ['GNB']
+
+    def process(self, input_data):
         pipes_result = {}
         for pname in self.pipes.keys():
+            print pname
             last_vector = input_data
             pipes_result[pname] = None
             for mname in self.pipes[pname]:
                 mod = PPlist[mname]
                 mod.resume()
+                print last_vector
                 last_vector = mod.convert(last_vector)
             pipes_result[pname] = last_vector
         return self.postprocess(pipes_result)
-            
-    def postprocess(pipes_result):
+
+    def postprocess(self, pipes_result):
         # Simple Merge
         res_list = []
         for res in pipes_result.keys():
             res_list.append(pipes_result[res])
         res_list.sort()
-        return res_list[0]    
+        print res_list[0]
+        return res_list[0]
+
+import numpy as np
+min_val = -999
+test_data = np.genfromtxt('../Data/WRM/RAW/TS.csv', delimiter=',')
+test_mat = test_data[[1],3:]
+test_mat[np.isnan(test_mat)] = min_val
+test_mat[np.isinf(test_mat)] = min_val
+test_lbl = test_data[[1],:3]
+
+ppl = Pipeline()
+ppl.load_pipes()
+ppl.process(test_mat)
