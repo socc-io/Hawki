ec7b2cf1fa353c3dca72e73ea7bd9e359790e5b2
becxer <becxer@beaver.com>
2016-09-06 01:42:41 +0900

    refactoring Predictor pipeline

---
+++ Predictor/Module/APVOCA/COEX_APVOCA.py
@@ -0,0 +1,15 @@
+import pickle
+
+# Predictor
+class APVOCA:
+    def __init__(self):
+        #Once Load module (only in start server)
+        pass
+        
+    def resume(self):
+        #Always load data (load each process)
+        pass
+
+    def convert(self, vector):
+        #Return ( x,y,z,confidence )
+        return vector
--- Predictor/pipeline.py
+++ Predictor/pipeline.py
@@ -1,73 +1,63 @@
+#
+# Pipeline managing bridge between prediction module & web rest server
+#
+# @Author becxer87
+# @Email becxer87@gmail.com
+#
 
-# Predictor
-class GNB:
+from Predictor.Module.GNB import COEX_GNB
+from Predictor.Module.APVOCA import COEX_APVOCA
+import numpy as np
 
-    def __init__(self):
-        import pickle
-        #Once Load module (only in start server)
-        with open('AlgoModel/model/gnb_x_0.pkl', 'rb') as f:
-            self.clf_x = pickle.load(f)
-        with open('AlgoModel/model/gnb_y_0.pkl', 'rb') as f:
-            self.clf_y = pickle.load(f)
-
-    def resume(self):
-        #Always load data (load each process)
-        pass
-
-    def convert(self, vector):
-        #Return ( x,y,z,confidence )
-        preds_x = self.clf_x.predict(vector)
-        preds_y = self.clf_y.predict(vector)
-        print preds_x
-        print preds_y
-        return preds_x[0], preds_y[0]
-
-PPlist = {
-    'GNB' : GNB()
+# Prepared Pipeline module list
+module_list = {
+    'COEX_GNB' : COEX_GNB.GNB(),
+    'COEX_APVOCA' : COEX_APVOCA.APVOCA()
 }
 
-##### BELOW IS PIPLINE #####
+pipeline_list = {
+    'COEX' : ['COEST_APVOCA', 'COEX_GNB'],
+    'COEX_SIMPLE' : ['COEX_GNB']
+}
 
+# PIPLINE Main Module
 class Pipeline:
 
     def __init__(self):
-        self.pipes = {}
-
-    def load_pipes(self):
-        # Export this to Config file
-        self.pipes['GNB'] = ['GNB']
+        self.main_pipe = None
+        self.main_name = None
+        
+    def load_pipe(self, pipe_name):
+        global pipeline_list
+        self.main_pipe = pipeline_list[pipe_name]
+        self.main_name = pipe_name
 
     def process(self, input_data):
-        pipes_result = {}
-        for pname in self.pipes.keys():
-            print pname
-            last_vector = input_data
-            pipes_result[pname] = None
-            for mname in self.pipes[pname]:
-                mod = PPlist[mname]
-                mod.resume()
-                print last_vector
-                last_vector = mod.convert(last_vector)
-            pipes_result[pname] = last_vector
-        return self.postprocess(pipes_result)
+        global module_list
+        print "[Pipeline : '", self.main_name, "'] is now processing..."
+        last_vector = input_data
+        pipe_result = []
+        for mname in self.main_pipe:
+            mod = module_list[mname]
+            mod.resume()
+            last_vector = mod.convert(last_vector)
+            pipe_result.append(last_vector)
+        return pipe_result[-1]
 
-    def postprocess(self, pipes_result):
-        # Simple Merge
-        res_list = []
-        for res in pipes_result.keys():
-            res_list.append(pipes_result[res])
-        res_list.sort()
-        print res_list[0]
-        return res_list[0]
+#################### Test code for predicting test #####################
 
-import numpy as np
 min_val = -999
-test_data = np.genfromtxt('../Data/WRM/RAW/TS.csv', delimiter=',')
+test_data = np.genfromtxt('Data/WRM/RAW/TS.csv', delimiter=',')
+
+test_lbl = test_data[[1],:3]
 test_mat = test_data[[1],3:]
 test_mat[np.isnan(test_mat)] = min_val
 test_mat[np.isinf(test_mat)] = min_val
-test_lbl = test_data[[1],:3]
 
 ppl = Pipeline()
-ppl.load_pipes()
-ppl.process(test_mat)
+ppl.load_pipe('COEX_SIMPLE')
+res = ppl.process(test_mat)
+
+print 'Test result : ', res
+
+########################################################################
