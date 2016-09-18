5273f7a72d63a1dd731e08068bda581c81b3c57a
young <siosio34@nate.com>
2016-09-18 14:56:29 +0900

    make collect button event

--- App/HakwiApp/app/src/main/java/com/example/jeong/httpclient/CollectorActivity.java
+++ App/HakwiApp/app/src/main/java/com/example/jeong/httpclient/CollectorActivity.java
@@ -7,8 +7,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import data.DataSource;
+import data.Json;
 
 /**
  * Created by Jeong on 2016-09-16.
@@ -31,14 +40,18 @@ public class CollectorActivity  extends Activity {
 
     //버튼클릭 이벤트
     //TODO: 버튼 클릭시 현재위치 수집기능(서버에 rssi 보내주기) 구현
-    public void collectorClicked(View v){
+    public void collectorClicked(View v) throws JSONException {
 
         WifiCollector wifiCollector = new WifiCollector(wifimanager);
         wifiScanResult = wifiCollector.getWIFIScanResult();
 
+        Json layer = new Json();
+
         HttpHandler httpHandler = new HttpHandler();
-        // TODO: 2016. 9. 17. 컬렉트 액티비티 시나리오 
-        // 1. Datasouce.createrequsturl 로 일단 url 을 만듬
+        // TODO: 2016. 9. 17. 컬렉트 액티비티 시나리오
+        String collectorUrl = DataSource.createRequestURL(DataSource.DATAFORMAT.RSSIDSET,0,0,0,0,null);
+        JSONObject rssiJsonObject = layer.createRssiJson("bid","x","y","z",wifiScanResult);
+        Log.i("rssijson test",rssiJsonObject.toString());
         // 2. Datasource.json 에 있는 함수를 이용해서 wifiscanResultstring 을 json형태로 담음
         // 3. 통합된 서버 http post 코드로 new GetContacts().execute(makeURL, "POST")방식으로 함
         // 4. 서버에 정상적으로 올려져있는지 확인하면 이기능 구현 끝
