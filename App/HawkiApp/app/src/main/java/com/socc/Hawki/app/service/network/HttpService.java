d0b56991c3b94516230ca4d9a0e9b1ce33290aa5
young <siosio34@nate.com>
2017-11-25 16:24:10 +0900

    Make load poidata List

--- App/HawkiApp/app/src/main/java/com/socc/Hawki/app/service/network/HttpService.java
+++ App/HawkiApp/app/src/main/java/com/socc/Hawki/app/service/network/HttpService.java
@@ -3,6 +3,7 @@ package com.socc.Hawki.app.service.network;
 import com.socc.Hawki.app.service.request.PostCollectRssiReq;
 import com.socc.Hawki.app.service.request.PostGetPositionReq;
 import com.socc.Hawki.app.service.response.GetBuildingInfoRes;
+import com.socc.Hawki.app.service.response.GetPoiListReq;
 import com.socc.Hawki.app.service.response.PostGetPositionRes;
 
 import org.json.JSONObject;
@@ -12,6 +13,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -30,4 +32,8 @@ public interface HttpService {
     @Headers("Content-type: application/json; charset=utf-8")
     @POST("getposition")
     Call<PostGetPositionRes> postGetPosition(@Body PostGetPositionReq req);
+
+    @Headers("Content-type: application/json; charset=utf-8")
+    @GET("building/{id}/poi")
+    Call<GetPoiListReq> getPOIList(@Path(value="id", encoded = true) String id);
 }
